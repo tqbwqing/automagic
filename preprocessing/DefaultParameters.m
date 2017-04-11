f7f6d4ccf206e95ac65d07e3364107cec9f38b7c
amirreza <abahrein@student.ethz.ch>
2017-04-11 10:56:26 +0200

    Comments for constant classes. Screenshots updated.

--- preprocessing/DefaultParameters.m
+++ preprocessing/DefaultParameters.m
@@ -1,4 +1,23 @@
 classdef DefaultParameters
+    %DefaultParameters is a class containing static constant variables used
+    %throughout the preprocessing. Note that not every field is a parameter 
+    %to preprocessing, some are simply constant values that are kept here
+    %for better maintenance.
+    %
+    % Copyright (C) 2017  Amirreza Bahreini, amirreza.bahreini@uzh.ch
+    % 
+    % This program is free software: you can redistribute it and/or modify
+    % it under the terms of the GNU General Public License as published by
+    % the Free Software Foundation, either version 3 of the License, or
+    % (at your option) any later version.
+    % 
+    % This program is distributed in the hope that it will be useful,
+    % but WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    % GNU General Public License for more details.
+    % 
+    % You should have received a copy of the GNU General Public License
+    % along with this program.  If not, see <http://www.gnu.org/licenses/>.
     properties(Constant)        
         filter_params = struct('notch_freq', 50, ...
                                 'notch_eu', 50, ...
--- src/ConstantGlobalValues.m
+++ src/ConstantGlobalValues.m
@@ -1,4 +1,21 @@
 classdef ConstantGlobalValues
+    %ConstantGlobalValues is a class containing all constant values used
+    %throughout the application.
+    %
+    % Copyright (C) 2017  Amirreza Bahreini, amirreza.bahreini@uzh.ch
+    % 
+    % This program is free software: you can redistribute it and/or modify
+    % it under the terms of the GNU General Public License as published by
+    % the Free Software Foundation, either version 3 of the License, or
+    % (at your option) any later version.
+    % 
+    % This program is distributed in the hope that it will be useful,
+    % but WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    % GNU General Public License for more details.
+    % 
+    % You should have received a copy of the GNU General Public License
+    % along with this program.  If not, see <http://www.gnu.org/licenses/>.
     properties(Constant)
         version = '1.4.0';
             
@@ -37,4 +54,3 @@ classdef ConstantGlobalValues
         end
     end
 end
-
