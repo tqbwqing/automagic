f2c14a025b397faa5d6c6e84cd6815d60132766d
amirreza <abahrein@student.ethz.ch>
2017-08-30 16:25:18 +0200

    new version

--- src/ConstantGlobalValues.m
+++ src/ConstantGlobalValues.m
@@ -18,7 +18,7 @@ classdef ConstantGlobalValues
     % along with this program.  If not, see <http://www.gnu.org/licenses/>.
     properties(Constant)
 
-        version = '1.4.4';
+        version = '1.4.5';
             
         DEFAULT_keyword = 'Default';
                 
